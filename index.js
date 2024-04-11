f3da939e81e7deb23f8c2107565b2d869246d5ac
Osbaldo <ocruz1998.oc@gmail.com>
2024-04-10 20:28:11 -0700

    set up html page and start of js

---
+++ index.js
@@ -0,0 +1,8 @@
+const quizQuestions = [
+    {
+        question: "What is the first casino to open on the Las Vegas strip?",
+        choices: ["Dunes", "Flamingo", "Stardust", "Riviera"],
+        correctAnswer: "Flamingo"
+    },
+]
+
