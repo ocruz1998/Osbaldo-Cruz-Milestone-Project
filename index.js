aa4f38a2e5f00ff2de3c45e5c91eb025dbd7252a
Osbaldo <ocruz1998.oc@gmail.com>
2024-04-10 20:43:11 -0700

    added questions on javascript

--- index.js
+++ index.js
@@ -4,5 +4,28 @@ const quizQuestions = [
         choices: ["Dunes", "Flamingo", "Stardust", "Riviera"],
         correctAnswer: "Flamingo"
     },
+
+    {
+        question: "When was the Welcome to Las Vegas sign created?",
+        choices: ["1956", "1957", "1958", "1959"],
+        correctAnswer: "1959"
+    },
+    {
+        question: "Which former casino did NOT exist on the strip?",
+        choices: ["Imperial Palace", "El Rancho", "Tangiers", "Aladdin"],
+        correctAnswer: "Tangiers"
+    },
+
+    {
+        question: "What was the first racially integrated casino in Las Vegas?",
+        choices: ["El Cortez", "Moulin Rouge", "Stardust", "Desert Inn"],
+        correctAnswer: "Flamingo"
+    },
+
+    {
+        question: "Are you drunk?",
+        choices: ["Yes", "No", "Maybe?", "sdfsdlgjsd"],
+        correctAnswer: "Moulin Rouge"
+    },
 ]
 
